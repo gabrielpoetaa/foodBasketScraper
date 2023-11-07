ea6e795899c03477bcf902d98836e5743fec6996
Gabriel Poeta <gugapoeta@gmail.com>
2023-11-07 17:05:31 -0500

    Update orange() URL

--- app.js
+++ app.js
@@ -118,6 +118,7 @@ function scrapeTimer() {
 let proxy = `http://${ipAdresses[randomNumber]}:${portNumbers[randomNumber]}`;
 console.log(proxy);
 
+
 scrapeCheese()
 .then(scrapeCheeseBlock)
 .then(mediumCheeseSlices)
@@ -1756,7 +1757,7 @@ function orange() {
         browser.close();
       }
       resolve(
-        scrapeUrl("https://www.nofrills.ca/navel-orange-medium/p/20287659001_KG")
+        scrapeUrl("https://www.nofrills.ca/navel-orange/p/20426078001_KG")
       );
       
     }, randomSecond());
